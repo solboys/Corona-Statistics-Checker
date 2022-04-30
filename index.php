38b9a212572cca10fb54fec8b2ff44386e8be584
solboys <tayyab910629@gmail.com>
2022-04-30 14:12:35 +0500

    API

--- index.php
+++ index.php
@@ -6,3 +6,15 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 if(isset($_GET['country']) && !empty($_GET['country'])) {
+
+	curl_setopt($ch, CURLOPT_URL, 'https://covid19.mathdro.id/api/countries/' . urlencode($_GET['country']));
+	$result = curl_exec($ch);
+	$data = json_decode($result, true);
+
+	curl_setopt($ch, CURLOPT_URL, 'https://covid19.mathdro.id/api');
+	$MainApi = curl_exec($ch);
+	$totalStatistics = json_decode($MainApi, true);
+
+}
+
+?>
